e587a1ee46800574c9809600c096012083ec69d2
ESSO <islamdiab967@gmail.com>
2023-01-12 20:22:54 +0200

    create a new component for hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero'>
+        <div className='content'>
+            <p>Call Us</p>
+            <p>1-800-123-456</p>
+            <p>Because money</p>
+            <p>Doesn't come with instructions</p>
+            <button className='button'>Free Consultation</button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
