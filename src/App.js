8e82ca693466389e16a6e22e1365cd0041395971
ESSO <islamdiab967@gmail.com>
2023-01-13 04:17:01 +0200

    import testimonials component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Testimonials />
     </div>
   );
 }
