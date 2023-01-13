69490781d79bd1907f539e9b7be7df6e6ec496b8
ESSO <islamdiab967@gmail.com>
2023-01-13 03:14:26 +0200

    import about component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import About from "./components/About/About";
 import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Hero />
+      <About />
     </div>
   );
 }
